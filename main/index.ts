3ac9b3f1d9afa1c06287484ab3c409c71cd07f2c
claraudofa <claracruzz@yahoo.co.uk>
2017-03-28 14:11:13 +0100

    make lib work with require 'fusionjs'

--- main/index.ts
+++ main/index.ts
@@ -2,4 +2,10 @@ import {FusionModel} from './model/FusionModel';
 import {FusionImmutable} from './model/FusionImmutable';
 import {FusionStore} from './store/FusionStore';
 
+const fuisonjs = {
+    FusionModel: FusionModel,
+    FusionImmutable: FusionImmutable,
+    FusionStore: FusionStore
+}
+export default {fuisonjs};
 export {FusionImmutable, FusionModel, FusionStore};
